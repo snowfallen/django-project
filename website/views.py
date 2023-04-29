d8bb88a4e24290c339913f9385c2a1b825b775d9
snowfallen <bondarenkoov1998@gmail.com>
2023-04-29 15:23:09 +0200

    #5 add urls for website, include basic view and create tempalate folder

--- website/views.py
+++ website/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from django.template import loader
+from .models import Menu
+
 
 # Create your views here.
+def index(request):
+    menu_item = Menu
+    context = {
+        'menu_item': menu_item
+    }
+    return render(request, 'website/index.html', context)
+
