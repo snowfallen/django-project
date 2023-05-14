a1f3221fe27f85f8ef85039cfdbf394c6c059010
snowfallen <bondarenkoov1998@gmail.com>
2023-05-14 19:09:40 +0200

    #7 change views

--- website/views.py
+++ website/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.template import loader
 from .models import Post
 
 
 # Create your views here.
-class Dashboard(generic.TemplateView):
-    template_name = 'website/dashboard.html'
-
+def posts(request):
+    post_list = get_object_or_404(Post)
+    return render(request, "website/dashboard.html", {"posts": post_list})
 
